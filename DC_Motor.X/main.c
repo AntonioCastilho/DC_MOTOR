726cb99d57aa218363d0450eba145baf400e7094
Antonio Castilho <castilhomec@gmail.com>
2022-07-16 18:34:06 -0300

    initial
    Start project

---
+++ DC_Motor.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Castilho
+ *
+ * Created on 16 de Julho de 2022, 18:06
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# 
+  Project: DC Motor Control with Dual Hall Effect Sensor
+  (In progress)
